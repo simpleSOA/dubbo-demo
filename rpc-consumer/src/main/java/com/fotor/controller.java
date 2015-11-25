2b5d39a713907a7bbecc6b37bf0a114aa1aaf18b
代码狂人 <467339640@qq.com>
2015-11-25 20:18:39 +0800

    provider 新增

--- rpc-consumer/src/main/java/com/fotor/controller.java
+++ rpc-consumer/src/main/java/com/fotor/controller.java
@@ -1,8 +1,20 @@
 package com.fotor;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.fotor.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class controller {
 
+    @Reference(version ="1.0.0")
+    private UserService userService;
+
+    @RequestMapping(value = "/dubbo/helloworld")
+    @ResponseBody
+    public String helloWorld(String name){
+        return userService.findUserByName(name);
+    }
 }
