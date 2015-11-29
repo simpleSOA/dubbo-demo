a8286ad7b1edda6bb934c1a263c48062faadde1e
linyang <123456>
2015-11-29 15:21:55 +0800

    更改配置文件

--- rpc-provider/src/main/java/com/fotor/service/impl/UserServiceImpl.java
+++ rpc-provider/src/main/java/com/fotor/service/impl/UserServiceImpl.java
@@ -1,12 +1,21 @@
 package com.fotor.service.impl;
 
 
+import com.fotor.dao.BaseDao;
 import com.fotor.model.User;
 import com.fotor.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private BaseDao baseDao;
+
     @Override
     public User findUserByName(String name) {
+        baseDao.get("userMapper.findUserByName",name);
         return null;
     }
 }
